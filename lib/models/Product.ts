16252c26c4b3a98eb5e05ab9041bebe62af53c1a
Shreyash Sagar <shreyashsagar2005@gmail.com>
2026-07-20 18:34:28 +0530

    Allow dynamic categories in Product model

--- lib/models/Product.ts
+++ lib/models/Product.ts
@@ -19,7 +19,7 @@ const ProductSchema: Schema = new Schema(
     price: { type: Number, required: true },
     image: { type: String, required: true },
     images: { type: [String], default: [] },
-    category: { type: String, required: true, enum: ['Birthday', 'Anniversary', 'Themed', 'Add-ons'] },
+    category: { type: String, required: true },
     isAddon: { type: Boolean, default: false }
   },
   {
